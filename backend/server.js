67f32e9e99d0b1c6ffad91aa23a59bf20acdf220
hunterwaters <hunterrwaters@gmail.com>
2020-09-14 20:06:52 -0700

    make it better

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import data from './data';
+import config from './config';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+ import userRoute from './routes/userRoute';
+ import productRoute from './routes/productRoute';
+ import orderRoute from './routes/orderRoute';
+
+ const path = require('path');
+
+
+ const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+}).catch (error => console.log(error.reason));
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/orders", orderRoute);
+app.get("/api/config/paypal", (req, res) => {
+    res.send(config.PAYPAL_CLIENT_ID);
+})
+
+
+app.listen(config.PORT, () => {
+    console.log("Server started at http://localhost:5000");
+})
+
+
+app.use(express.static(path.join(__dirname, '/../frontend/build')))
+
+app.get('*', (req, res) =>
+    res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`)));
+
---
+++ dist/config.js
@@ -0,0 +1,20 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _dotenv = _interopRequireDefault(require("dotenv"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_dotenv.default.config();
+
+var _default = {
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/stitchworks',
+  JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
+  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
+  PORT: process.env.PORT || 5000
+};
+exports.default = _default;
