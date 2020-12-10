065fcb5446594437bf798029c9ecd85f0876f138
hunterwaters <hunterrwaters@gmail.com>
2020-12-09 18:21:23 -0800

    final commit

--- backend/server.js
+++ backend/server.js
@@ -25,10 +25,14 @@ app.get("/api/config/paypal", (req, res) => {
 })
 
 
-app.use(express.static(path.join(__dirname, '/../frontend/build')));
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
-});
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+})
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(config.PORT, () => {
   console.log('Server started at http://localhost:5000');
